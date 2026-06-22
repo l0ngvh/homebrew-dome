1c3f9348be56f1f3b89607dd2ac79ae63554ff33
vohoanglong0107 <vo.hoang.long@outlook.com>
2026-06-22 21:47:46 +0900

    feat: nightly build

--- Casks/dome-nightly.rb
+++ Casks/dome-nightly.rb
@@ -1,12 +1,12 @@
-cask "dome" do
+cask "dome-nightly" do
   arch arm: "aarch64-apple-darwin"
 
-  version "0.1.0"
-  sha256 arm: "PLACEHOLDER_AARCH64_SHA256"
+  version "nightly"
+  sha256 :no_check
 
-  url "https://github.com/l0ngvh/dome/releases/download/v#{version}/dome-#{version}-#{arch}.dmg"
-  name "Dome"
-  desc "Cross-platform tiling window manager"
+  url "https://github.com/l0ngvh/dome/releases/download/nightly/dome-nightly-#{arch}.dmg"
+  name "Dome (Nightly)"
+  desc "Cross-platform tiling window manager (nightly build)"
   homepage "https://github.com/l0ngvh/dome"
 
   postflight do
