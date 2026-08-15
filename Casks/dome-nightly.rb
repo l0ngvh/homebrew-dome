8f7e9e911d0af4ac3b491b2efe3a341fbc42e84a
Vo Hoang Long <longvh@amazon.com>
2026-08-15 19:16:26 +0900

    fix: 2 nightly same day causes hash mismatch

--- Casks/dome-nightly.rb
+++ Casks/dome-nightly.rb
@@ -4,7 +4,7 @@ cask "dome-nightly" do
   version "2026.08.15.0541a1c"
   sha256 arm:   "1fc26f9e445b50b52f4a2f4f4a43e154927665720974f34762a4415f2d81f053"
 
-  url "https://github.com/l0ngvh/dome/releases/download/nightly/dome-nightly-#{arch}.dmg"
+  url "https://github.com/l0ngvh/dome/releases/download/nightly-#{version}/dome-nightly-#{version}-#{arch}.dmg"
   name "Dome (Nightly)"
   desc "Cross-platform tiling window manager (nightly build)"
   homepage "https://github.com/l0ngvh/dome"
